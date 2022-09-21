263c53b96a563a2dcafc3d9976a401cc61f1a65a
guilhermezanella <guihdzanella@gmail.com>
2022-09-21 11:10:57 -0300

    formatando codigo add clientmodel

--- mod_cliente/ClienteModel.py
+++ mod_cliente/ClienteModel.py
@@ -2,21 +2,23 @@ import db
 from sqlalchemy import Column, VARCHAR, CHAR, Integer
 
 # ORM
+
 class ClienteDB(db.Base):
-    __tablename__ = 'tb_funcionario'
-    id_funcionario = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    __tablename__ = 'tb_cliente'
+    id_cliente = Column(Integer, primary_key=True,
+                        autoincrement=True, index=True)
     nome = Column(VARCHAR(100), nullable=False)
-    matricula = Column(CHAR(10), nullable=False)
     cpf = Column(CHAR(11), unique=True, nullable=False)
     telefone = Column(CHAR(11), nullable=False)
-    grupo = Column(Integer, nullable=False)
+    compra_fiado = Column(Integer, nullable=False)
+    dia_fiado = Column(Integer, nullable=False)
     senha = Column(VARCHAR(200), nullable=False)
 
-def __init__(self, id_funcionario, nome, matricula, cpf, telefone, grupo, senha):
-    self.id_funcionario = id_funcionario
-    self.nome = nome
-    self.matricula = matricula
-    self.cpf = cpf
-    self.telefone = telefone
-    self.grupo = grupo
-    self.senha = senha
+    def __init__(self, id_cliente, nome, cpf, telefone, compra_fiado, dia_fiado, senha):
+        self.id_cliente = id_cliente
+        self.nome = nome
+        self.cpf = cpf
+        self.telefone = telefone
+        self.compra_fiado = compra_fiado
+        self.dia_fiado = dia_fiado
+        self.senha = senha
